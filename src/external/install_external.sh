11b8f10590f905fc8f74390422902d692feedcf3
BNZhao <barney.zhao@qq.com>
2019-06-10 17:34:37 +0800

    set env

--- src/external/install_external.sh
+++ src/external/install_external.sh
@@ -2,14 +2,15 @@
 echo "Cloning Neural Mesh Renderer"
 git clone https://github.com/daniilidis-group/neural_renderer.git
 cd neural_renderer
-# This seems to be the last commit with Pytorch 0.4.0 support.
-git reset --hard 55a05a
+# This seems to be the last commit with Pytorch 0.4.0 support.(0.4.0 have problems with Alphapose)
+# git reset --hard 55a05a
 python setup.py install
 cd ..
 
 echo "Cloning AlphaPose"
-git clone https://github.com/akanazawa/AlphaPose
+# better using the newest AlphaPose
+https://github.com/MVIG-SJTU/AlphaPose.git
 cd AlphaPose
-git checkout pytorch
+# git checkout pytorch
 pip install -r requirements.txt
 pip install -r PoseFlow/requirements.txt
